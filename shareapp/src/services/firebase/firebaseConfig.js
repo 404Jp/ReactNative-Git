f28ab36ea9f5e8efa6db7072fc602b4192dc60dd
JPBouff <137674114+JPBouff@users.noreply.github.com>
2023-07-04 00:08:06 -0400

    sin claves firebase

--- shareapp/src/services/firebase/firebaseConfig.js
+++ shareapp/src/services/firebase/firebaseConfig.js
@@ -8,15 +8,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCusNvCnz1hFv8do14TSAepE-0HJjbOMkc",
-  authDomain: "sharefood-beta-20231.firebaseapp.com",
-  databaseURL: "https://sharefood-beta-20231-default-rtdb.firebaseio.com",
-  projectId: "sharefood-beta-20231",
-  storageBucket: "sharefood-beta-20231.appspot.com",
-  messagingSenderId: "475833819231",
-  appId: "1:475833819231:web:11c2b64149eef48a19fbea",
-  measurementId: "G-ZB3QCMR5TB",
-  databaseURL: "https://sharefood-beta-20231-default-rtdb.firebaseio.com/",
+
 };
 
 // Initialize Firebase
