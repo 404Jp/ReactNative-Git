abcabe77809350f8cbcf5ee84eba9a5d6b9cc370
Tomas Ibañez <98287990+Kirobits@users.noreply.github.com>
2023-07-04 18:50:56 -0400

    UserScreen2

--- shareapp/src/screens/UserScreen.tsx
+++ shareapp/src/screens/UserScreen.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 interface UserInformationProps {
   fullName: string;
   email: string;
   password: string;
-  Titulo: string
+  Titulo: string;
 }
 
-const UserInformation: React.FC<UserInformationProps> = ({ fullName, email, password, Titulo }) => {
+const UserInformation: React.FC<UserInformationProps> = ({
+  fullName,
+  email,
+  password,
+  Titulo,
+  }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const cerrarsesion = () =>{
+  
+  }
+  
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>FoodShare</Text>
@@ -21,7 +37,16 @@ const UserInformation: React.FC<UserInformationProps> = ({ fullName, email, pass
       <Text style={styles.text}>{email}</Text>
 
       <Text style={styles.subtitulo}>Contraseña:</Text>
-      <Text style={styles.text}>{password}</Text>
+      <View style={styles.passwordContainer}>
+        <Text style={styles.passwordText}>{showPassword ? password : '******'}</Text>
+        <TouchableOpacity onPress={toggleShowPassword}>
+          <Text style={styles.mostrarButton}>{showPassword ? 'Ocultar' : 'Mostrar'}</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity onPress={cerrarsesion}>
+        <Text style={styles.cerrarsesion}>Cerrar Sesion</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -33,16 +58,39 @@ const styles = StyleSheet.create({
   },
   subtitulo: {
     marginBottom: 5,
-    marginLeft:7,
+    marginLeft: 7,
     fontSize: 16,
     fontWeight: 'bold',
   },
   title2: {
     textAlign: 'center',
     fontSize: 25,
-    fontWeight:'bold',
+    fontWeight: 'bold',
     color: 'gray',
     marginBottom: 40,
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  passwordText: {
+    flex: 1,
+    fontSize: 16,
+    padding: 10,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderRadius: 30,
+  },
+  mostrarButton: {
+    marginLeft: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    backgroundColor: '#39558C',
+    borderRadius: 30,
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   correo: {
     fontSize: 16,
@@ -74,6 +122,15 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     color: '#39558C',
     marginTop:10,
+  },
+  cerrarsesion: {
+    marginTop: 10,
+    fontSize: 16,
+    textAlign: 'center',
+    backgroundColor: '#39558C',
+    height: 30,
+    borderRadius:30,
+    color:'white',
   }
 });
 
