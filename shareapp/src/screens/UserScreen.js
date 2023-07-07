8c24cbb108cf8909c32838284ab21b6613bef3d5
JPBouff <137674114+JPBouff@users.noreply.github.com>
2023-07-06 20:17:27 -0400

    Update UserScreen con informacion
    Cruce de informacion realtime y authentication por email

--- shareapp/src/screens/UserScreen.js
+++ shareapp/src/screens/UserScreen.js
@@ -1,23 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-import { auth } from '../services/firebase/firebaseConfig';
-
+import { auth, database } from '../services/firebase/firebaseConfig';
+import { ref, onValue } from 'firebase/database';
 const UserScreen = () => {
   const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     const currentUser = auth.currentUser;
 
     if (currentUser) {
-     
       setUser(currentUser);
+
+      // Obtener datos del usuario utilizando el email como referencia
+      const usersRef = ref(database, 'Users');
+      onValue(usersRef, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          const user = Object.values(data).find(
+            (userData) => userData.email === currentUser.email
+          );
+          setUserData(user);
+        }
+      });
     }
   }, []);
 
   return (
     <View>
-      {user ? (
-        <Text>{JSON.stringify(user, null, 2)}</Text>
+      {user && userData ? (
+        <View>
+          <Text>Email: {userData.email}</Text>
+          <Text>Nombre: {userData.name}</Text>
+          <Text>Edad: {userData.age}</Text>
+          <Text>Publicación:</Text>
+          <Text>Título: {userData.post.title}</Text>
+          <Text>Descripción: {userData.post.description}</Text>
+          <Text>Ubicación:</Text>
+          <Text>Latitud: {userData.post.location.latitude}</Text>
+          <Text>Longitud: {userData.post.location.longitude}</Text>
+        </View>
       ) : (
         <Text>Sin usuario.</Text>
       )}
